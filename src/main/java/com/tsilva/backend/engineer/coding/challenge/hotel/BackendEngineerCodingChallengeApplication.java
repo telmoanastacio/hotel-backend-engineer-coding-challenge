0f9231d0419522b7cebe7bb800f58e7c88ce4dbb
Telmo Silva <telmo.anastacio@gmail.com>
2025-02-21 15:34:11 +0100

    spring initializr

---
+++ src/main/java/com/tsilva/backend/engineer/coding/challenge/hotel/BackendEngineerCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.tsilva.backend.engineer.coding.challenge.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEngineerCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEngineerCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsilva/backend/engineer/coding/challenge/hotel/BackendEngineerCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsilva.backend.engineer.coding.challenge.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEngineerCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
